a8893cab8a56dd6fb65d5ce9c424caded7dfb802
Diego <dvillarroel82@gmail.com>
2020-09-30 09:11:00 -0300

    agregar los comentarios e imagen para la persona

---
+++ database/migrations/2020_09_30_113251_create_calificaciones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalificacionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calificaciones', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('persona_id');
+            $table->smallInteger('precio');
+            $table->smallInteger('tiempo_del_trabajo');
+            $table->smallInteger('presentismo');
+            $table->text('comentario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calificaciones');
+    }
+}

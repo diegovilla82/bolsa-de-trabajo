2306b8617664ad83d64dd37c0362b22681d19dd2
Diego <dvillarroel82@gmail.com>
2020-10-05 09:35:24 -0300

    vista create de usuario

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -7,7 +7,7 @@
 
         <div class="card-body">
         <h5 class="card-title">Crear Usuario</h5>
-            {!! Form::open(['route' => 'users.store', 'method'=>'posts', 'files' => true]) !!}
+            {!! Form::open(['route' => 'users.store', 'method'=>'posts', 'files' => true,  ,'user'=> new App\User]) !!}
                 @include('admin.user.partials.form')
                 {{ Form::submit('Crear', ['class' => 'btn btn-primary btn-block mt-3'])}}
             {!! Form::close() !!}
